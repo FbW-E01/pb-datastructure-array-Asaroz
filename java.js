bf25c1ada910c7f532ed2dd6dd1cb34bf7ef29f9
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-04-13 11:58:27 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,6 @@
+let my_array = ['Apple', 'Banana','Strawberry','Blueberry','Orange']
+console.log(my_array)
+let my_third_item = my_array[2]
+console.log(my_third_item)
+my_array[2] = 'The third item'
+console.log(my_array)
